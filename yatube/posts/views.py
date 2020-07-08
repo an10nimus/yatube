1c03da1f2d6a7a557ca8ed3d8f775d4f91586935
Anton Oskirko <oskirych@gmail.com>
2020-07-09 00:33:55 +0300

    Sprint 3 result

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-# одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by('-pub_date')[:11]
-    # собираем тексты постов в один, разделяя новой строкой
-    # output = []
-    # for item in latest:
-    #     output.append(item.text)
-    # return HttpResponse('\n'.join(output))
+    latest = Post.objects.all()[:11]  # проверил, тесты проходят без all, баг)
     return render(request, 'index.html', {'posts': latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:12]
+    return render(request, 'group.html', {'group': group, 'posts': posts})
